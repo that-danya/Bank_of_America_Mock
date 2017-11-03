363ea80ae44a41f671b80f534c1be9d5d527a1d7
Danya Dye <danya.dye@gmail.com>
2017-11-02 18:57:46 -0700

    Working Bank of America Mock sign in page.

--- server.py
+++ server.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
 from jinja2 import StrictUndefined
+from flask import Flask, render_template, request, flash, redirect, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+
+import os
+import re
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 # add key for debug
-app.secret_key = 'BOA Portal Demo'
+app.secret_key = 'duuuuude. this is an app!!'
 
 # debugger please yell at me if I do something weird
 app.jinja_env.undefined = StrictUndefined
 app.jinja_env.auto_reload = True
 
 
-#landing page
 @app.route('/')
 def index():
     """Homepage with logon portal."""
@@ -20,14 +23,15 @@ def index():
     return render_template('homepage.html')
 
 
-@app.route()
+@app.route('/application')
 def loan_application():
     """Show loan application with uploading capability."""
 
     return render_template('loan_app.html')
 
 
-#####################################
-
+# if running this page, run debugger, load to host
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    DebugToolbarExtension(app)
+    app.run(debug=True, host="0.0.0.0")
